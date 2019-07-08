feec65b495386f4a452a57b389c815002a43822c
pzq <pzq797933@163.com>
2019-07-08 09:41:31 +0800

    Revert "upload:url_base64"
    This reverts commit 18c2d36d

--- HandwrittenArithmeticExpressionRecognizer/static/HandwrittenArithmeticExpressionRecognizer/js/upload.js
+++ HandwrittenArithmeticExpressionRecognizer/static/HandwrittenArithmeticExpressionRecognizer/js/upload.js
@@ -1,8 +1,6 @@
 let img_url_base64 = "";
 
 window.onload = function () {
-    // 获取文件上传组件
-    let fp = document.getElementById("file");
     // 获取识别结果框
     let res1 = document.getElementById("res1");
     // 获取计算结果框
@@ -24,7 +22,6 @@ window.onload = function () {
     });
     // 清除按钮绑定事件
     $("#clear").click(function () {
-        fp.value = "";
         img.src = "";
         clear();
     });
